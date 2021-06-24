9d857aefc976931370bb5a9a121306a1edefa940
transient91 <1454597191@qq.com>
2021-06-24 21:47:05 +0800

    Site updated: 2021-06-24 21:47:05

--- code/可重定位目标文件链接时对.rodata节数据的重定位/main.c
+++
@@ -1,9 +0,0 @@
-/* main.c */
-#include<stdio.h>
-int a[4] = { -1,-2,3,4 };
-extern int val;
-int sum();
-int main() {
-    val = sum();
-    printf("sum=%d\n", val);
-}
--- code/可重定位目标文件链接时对.rodata节数据的重定位/test.c
+++
@@ -1,9 +0,0 @@
-/* test.c */
-extern int a[];
-int val = 0;
-int sum() {
-    int i;
-    for (i = 0;i < 4;i++)
-        val += a[i];
-    return val;
-}
